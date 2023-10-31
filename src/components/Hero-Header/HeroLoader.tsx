0dcdd54975a982589684a3ae5d0efb4522bb61ac
Yurii Novikov <urichcoolx@gmail.com>
2023-10-31 12:26:20 +0100

    finished-hero-header

--- src/components/Hero-Header/HeroLoader.tsx
+++ src/components/Hero-Header/HeroLoader.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react';
-import { Audio } from 'react-loader-spinner';
+import { Oval } from 'react-loader-spinner';
 
 const HeroLoader:FC = () => {
     return (
-      <div className='container'>
+      <div className="container">
         <div className="hero-loader">
-          <Audio height="250" width="250" color="#fff" />
+          <Oval
+            height={200}
+            width={200}
+            color="#fff"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            ariaLabel="oval-loading"
+            secondaryColor="#fff"
+            strokeWidth={2}
+            strokeWidthSecondary={2}
+          />
         </div>
       </div>
     );
