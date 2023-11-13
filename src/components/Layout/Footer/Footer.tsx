409f6c922440a98a741686c0b3a2b00814941de5
Yurii Novikov <urichcoolx@gmail.com>
2023-11-13 01:31:41 +0100

    finished-footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 import AudiophileIcon from "../../../images/icons/AudiophileIcon";
 import { Link, NavLink } from "react-router-dom";
+import FacebookIcon from "../../../images/icons/FacebookIcon";
+import TwitterIcon from "../../../images/icons/TwitterIcon";
+import InstagramIcon from "../../../images/icons/InstagramIcon";
 
 const Footer: FC = () => {
   return (
     <footer className="footer">
-      <div className="footer-line"></div>
       <div className="container">
+        <div className="footer-line"></div>
         <Link to={"/"} className="footer-audiophile-logo">
           <AudiophileIcon />
         </Link>
@@ -36,12 +39,44 @@ const Footer: FC = () => {
             Earphones
           </NavLink>
         </nav>
-        <p>
+        <p className="Body-manrope-medium footer-description">
           Audiophile is an all in one stop to fulfill your audio needs. We're a
           small team of music lovers and sound specialists who are devoted to
           helping you get the most out of personal audio. Come and visit our
           demo facility - we’re open 7 days a week.
         </p>
+        <p className="Body-manrope-medium footer-description">
+          Copyright 2021. All Rights Reserved
+        </p>
+        <ul className="footer-socials-list">
+          <li className="footer-socials-list-item">
+            <a
+              href="https://github.com/Urichcool"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FacebookIcon />
+            </a>
+          </li>
+          <li className="footer-socials-list-item">
+            <a
+              href="https://github.com/Urichcool"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <TwitterIcon />
+            </a>
+          </li>
+          <li className="footer-socials-list-item">
+            <a
+              href="https://github.com/Urichcool"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <InstagramIcon />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
