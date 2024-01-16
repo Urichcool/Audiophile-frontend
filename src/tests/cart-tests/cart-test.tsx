7c23a41a134b2b253cd588ff807817eaf2183f86
Yurii Novikov <urichcoolx@gmail.com>
2024-01-17 00:32:23 +0100

    created-cart-test-file

---
+++ src/tests/cart-tests/cart-test.tsx
@@ -0,0 +1,10 @@
+import App from "../../App";
+import { screen, fireEvent } from "@testing-library/react";
+import { renderWithReduxAndRouter } from "../../tests/helpers/RenderWithReduxAndRouter";
+
+
+describe("cart-tests", () => {
+    test("should-open-cart-modal", () => {
+        
+    })
+})
