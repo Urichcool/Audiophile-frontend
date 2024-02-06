14e9ee50e784b65b6192222d0ee9cf7af73bf614
Yurii Novikov <urichcoolx@gmail.com>
2024-02-06 01:52:06 +0100

    created-checkout-page-and-checkout-container

---
+++ src/components/Checkout/CheckoutContainer.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react';
+
+const CheckOutContainer:FC = () => {
+    return (
+      <div className="checkout-container">
+        <h5 className="checkout-container-title">Checkout</h5>
+      </div>
+    );
+}
+
+export default CheckOutContainer;
--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,7 +1,16 @@
 import { FC } from "react";
+import GoBackButton from "../components/Reusable-Components/Buttons/GoBackButton";
+import CheckOutContainer from "../components/Checkout/CheckoutContainer";
 
 const CheckoutPage: FC = () => {
-  return <div></div>;
+  return (
+    <section className="checkout-page">
+      <div className="container">
+        <GoBackButton />
+        <CheckOutContainer/>
+      </div>
+    </section>
+  );
 };
 
 export default CheckoutPage;
