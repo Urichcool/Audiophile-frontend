d502169efea485619d6e85e9342e0da494150480
Yurii Novikov <urichcoolx@gmail.com>
2024-02-20 00:31:58 +0100

    added-regex-matches-to-validation-schema

--- src/components/Checkout/CheckoutForm.tsx
+++ src/components/Checkout/CheckoutForm.tsx
@@ -64,16 +64,28 @@ const CheckOutForm: FC = () => {
     },
     ""
   > = Yup.object().shape({
-    name: Yup.string().required("Required"),
-    email: Yup.string().email("Invalid email").required("Required"),
-    phone: Yup.string().required("Required"),
-    adress: Yup.string().required("Required"),
-    zip: Yup.string().required("Required"),
-    city: Yup.string().required("Required"),
-    country: Yup.string().required("Required"),
-    radioValue: Yup.string().required("Required"),
-    eMoneyNumber: Yup.string().required("Required"),
-    eMoneyPin: Yup.string().required("Required"),
+    name: Yup.string().required("This field is required"),
+    email: Yup.string()
+      .email("Incorrect email format")
+      .required("This field is required"),
+    phone: Yup.string()
+      .required("This field is required")
+      .matches(/^\+\d{1,4}\d{1,14}$/),
+    adress: Yup.string()
+      .required("This field is required")
+      .matches(/^[A-Za-z0-9\s\-.,'()/]+$/),
+    zip: Yup.string()
+      .required("This field is required")
+      .matches(/^d{5}(?:[-s]d{4})?$/),
+    city: Yup.string()
+      .required("This field is required")
+      .matches(/^[A-Za-z\s-']+$/),
+    country: Yup.string()
+      .required("This field is required")
+      .matches(/^[A-Za-z\s-']+$/),
+    radioValue: Yup.string().required("This field is required"),
+    eMoneyNumber: Yup.string().required("This field is required"),
+    eMoneyPin: Yup.string().required("This field is required"),
   });
 
   return (
@@ -82,12 +94,14 @@ const CheckOutForm: FC = () => {
       onSubmit={(values: ICheckOutFormValues) => {
         console.log(values);
       }}
-validationSchema={CheckOutValidationSchema}
+      validationSchema={CheckOutValidationSchema}
+      validateOnChange
     >
       {(props) => (
         <Form className="checkout-form">
           <div className="checkout-container">
             <h5 className="checkout-container-title">Checkout</h5>
+            {props.errors.phone ? <p>{props.errors.country}</p> : null}
             <BillingDetailsFields values={props.values} />
             <ShippingInfoFields values={props.values} />
             <PaymentDetailsFields
@@ -101,7 +115,7 @@ validationSchema={CheckOutValidationSchema}
             )}
           </div>
           <Summary />
-          <PersistFormikValues name="checkout-form" />
+          {/* <PersistFormikValues name="checkout-form" /> */}
         </Form>
       )}
     </Formik>
