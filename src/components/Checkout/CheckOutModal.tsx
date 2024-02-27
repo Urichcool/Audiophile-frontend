0d11c9d7b69429f107468859b7851ec3a152d707
Yurii Novikov <104870932+Urichcool@users.noreply.github.com>
2024-02-27 22:48:22 +0100

    created-checkout-ok-circle-and-title

--- src/components/Checkout/CheckOutModal.tsx
+++ src/components/Checkout/CheckOutModal.tsx
@@ -9,6 +9,7 @@ import {
   useAppSelector,
 } from "../../redux/reduxHooks/reduxHooks";  
 import { AnyAction } from "@reduxjs/toolkit";
+import CheckOutOkIcon from "../../images/icons/CheckOutOkIcon";
 
 const CheckOutModal:FC = () => {
   const isCheckOutModalOpen: boolean = useAppSelector(
@@ -31,7 +32,15 @@ const CheckOutModal:FC = () => {
               ? "checkout-modal-container-open"
               : "checkout-modal-container-close"
           }
-        ></div>
+        >
+          <div className="checkout-modal-container-ok-circle">
+            <CheckOutOkIcon />
+          </div>
+          <h2 className="checkout-modal-container-title">
+            THANK YOU
+            <br />FOR YOUR ORDER
+          </h2>
+        </div>
       </>
     );
 };
