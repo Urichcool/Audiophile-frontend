52f6104d34d92b6e81325836397bae61f42f1e52
Yurii Novikov <urichcoolx@gmail.com>
2024-03-04 16:56:01 +0100

    added-nav-button-to-cart-items

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -1,6 +1,14 @@
-import { FC } from "react";
+import { Dispatch, FC } from "react";
 import { priceWithCommas } from "../../utils/priceWithCommas";
 import CartQuantityButtons from "./CartQuantityButtons";
+import { NavigateFunction, useNavigate } from "react-router-dom";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../redux/reduxHooks/reduxHooks";
+import { selectIsCartModalOpen } from "../../redux/slices/cart/selectors";
+import { AnyAction } from "@reduxjs/toolkit";
+import { switchCartModal } from "../../redux/slices/cart/cartSlice";
 
 interface ICartItemProps {
   id: string;
@@ -17,18 +25,32 @@ const CartItem: FC<ICartItemProps> = ({
   quantity,
   price,
   picture,
-  totalPrice
+  totalPrice,
 }) => {
+  const navigate: NavigateFunction = useNavigate();
+  const isCartModalOpen: boolean = useAppSelector(selectIsCartModalOpen);
+  const dispatch: Dispatch<AnyAction> = useAppDispatch();
+  const handleOnNavButtonClick = (id: string): void => {
+    dispatch(switchCartModal(!isCartModalOpen))
+    navigate(`/${id}`);
+  };
   return (
     <li className="cart-list-item" data-testid="cart-item">
-      <div className="cart-list-item-picture-container">
-        <img src={picture} alt={name} className="cart-item-picture" />
-      </div>
-      <div className="cart-list-item-text-container">
-        <h6 className="cart-item-name">{name}</h6>
-        <p className="cart-item-price">{priceWithCommas(totalPrice)}</p>
-      </div>
-      <CartQuantityButtons id={id} quantity={quantity} price={price}/>
+      <button
+        className="cart-list-item-nav-button"
+        onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
+          handleOnNavButtonClick(id)
+        }
+      >
+        <div className="cart-list-item-picture-container">
+          <img src={picture} alt={name} className="cart-item-picture" />
+        </div>
+        <div className="cart-list-item-text-container">
+          <h6 className="cart-item-name">{name}</h6>
+          <p className="cart-item-price">{priceWithCommas(totalPrice)}</p>
+        </div>
+      </button>
+      <CartQuantityButtons id={id} quantity={quantity} price={price} />
     </li>
   );
 };
