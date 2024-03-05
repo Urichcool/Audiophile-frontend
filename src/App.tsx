5f4be277dab576d935e3a55a7221f2d0979582ea
Yurii Novikov <urichcoolx@gmail.com>
2024-03-06 00:36:25 +0100

    created-unknown -route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { FC } from "react";
-import {
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
 import HeadphonesPage from "./pages/HeadphonesPage";
@@ -11,40 +7,20 @@ import SpeakersPage from "./pages/SpeakersPage";
 import EarphonesPage from "./pages/EarphonesPage";
 import ProductIdPage from "./pages/ProductIdPage";
 import CheckoutPage from "./pages/CheckoutPage";
-import { useAppSelector } from "./redux/reduxHooks/reduxHooks";
-import { selectCartProducts } from "./redux/slices/cart/selectors";
 
 const App: FC = () => {
-  const cartProducts:
-    | {
-        id: string;
-        name: string;
-        quantity: number;
-        price: number;
-        picture: string;
-        totalPrice: number;
-      }[]
-    | [] = useAppSelector(selectCartProducts);
-  
   return (
     <Routes>
       <Route element={<Layout />} path="/">
-        <Route element={<ProductIdPage />} path="/:productId" />
+        <Route element={<ProductIdPage/>} path="/:productId"/>
         <Route index element={<HomePage />} />
         <Route element={<HeadphonesPage />} path="headphones" />
         <Route element={<SpeakersPage />} path="speakers" />
         <Route element={<EarphonesPage />} path="earphones" />
-        <Route
-          element={
-            cartProducts.length !== 0 ? (
-              <CheckoutPage />
-            ) : (
-                <Navigate replace to={'/'} />
-            )
-          }
-          path="checkout"
-        />
+        <Route element={<CheckoutPage />} path="checkout" />
       </Route>
+      <Route path="*" element={<Navigate to={'/'}/>}/>
+
     </Routes>
   );
 };
--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,16 +1,38 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import GoBackButton from "../components/Reusable-Components/Buttons/GoBackButton";
 import CheckOutForm from "../components/Checkout/CheckoutForm";
 import CheckOutModal from "../components/Checkout/CheckOutModal";
+import { useAppSelector } from "../redux/reduxHooks/reduxHooks";
+import { selectCartProducts } from "../redux/slices/cart/selectors";
+import { useNavigate } from "react-router-dom";
 
 const CheckoutPage: FC = () => {
+  const cartProducts:
+    | {
+        id: string;
+        name: string;
+        quantity: number;
+        price: number;
+        picture: string;
+        totalPrice: number;
+      }[]
+    | [] = useAppSelector(selectCartProducts);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (cartProducts.length === 0) {
+        navigate(-1);
+      }
+    }, 200);
+  }, [cartProducts.length, navigate]);
 
   return (
     <section className="checkout-page">
       <div className="container">
         <GoBackButton />
         <CheckOutForm />
-        <CheckOutModal/>
+        <CheckOutModal />
       </div>
     </section>
   );
