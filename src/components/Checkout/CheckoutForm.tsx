00243de2afdb385f821a4cc080a0d30c01a5125c
Yurii Novikov <urichcoolx@gmail.com>
2024-05-02 00:47:28 +0200

    added-stock-checking-on-submit-function

--- src/components/Checkout/CheckoutForm.tsx
+++ src/components/Checkout/CheckoutForm.tsx
@@ -20,21 +20,53 @@ import {
   selectIsCheckOutModalOpen,
   switchCheckOutModal,
 } from "../../redux/slices/checkout-modal/checkOutModalSlice";
-import { AnyAction } from "@reduxjs/toolkit";
+import { AnyAction, SerializedError } from "@reduxjs/toolkit";
 import { scrollUpFunc } from "../../utils/scrollUpFunc";
+import { selectCartProducts } from "../../redux/slices/cart/selectors";
+import { useCheckGoodsCartStockMutation } from "../../redux/services/goods";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 
 const CheckOutForm: FC = () => {
   const dispatch: Dispatch<AnyAction> = useAppDispatch();
   const isCheckOutModalOpen: boolean = useAppSelector(
     selectIsCheckOutModalOpen
   );
+  const cartProducts:
+    | {
+        id: string;
+        name: string;
+        quantity: number;
+        price: number;
+        picture: string;
+        totalPrice: number;
+        category: string;
+      }[]
+    | [] = useAppSelector(selectCartProducts);
+  const [checkCartStock, { isLoading }] = useCheckGoodsCartStockMutation();
+
   return (
     <Formik
       initialValues={initialValues}
-      onSubmit={(values: ICheckOutFormValues) => {
-        console.log(values);
-        scrollUpFunc();
-        dispatch(switchCheckOutModal(!isCheckOutModalOpen));
+      onSubmit={async (values: ICheckOutFormValues) => {
+        const result:
+          | {
+              data: {
+                isEnoughCartStock: boolean;
+              };
+            }
+          | {
+              error: FetchBaseQueryError | SerializedError;
+            } = await checkCartStock(
+          cartProducts.map(({ id, quantity }) => {
+            return { id: id, quantity: quantity };
+          })
+          );
+        if (Object.values(result)[0].isEnoughCartStock) {
+            console.log(values);
+            scrollUpFunc();
+            dispatch(switchCheckOutModal(!isCheckOutModalOpen));
+         }
+       
       }}
       validationSchema={CheckOutValidationSchema}
       validateOnBlur={false}
